31f05ee4d83b7d5ea93b493923b357a0db0a1c4e
basroy <60724444+basroy@users.noreply.github.com>
2020-02-06 23:00:14 -0800

    Add files via upload

--- bapp.py
+++ bapp.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+import sys
 
 app = Flask(__name__)
+app = Flask(__name__, template_folder='template') 
 
 @app.route('/')
 def index():
-    return "Hello, world"
+    return render_template('index.html')
+
+def prarg():
+    if len(sys.argv) >= 2:
+        name = sys.argv[1]
+    else:
+        name = 'Default'
+    print("ENtered" + name) 
 
 if __name__ == "__main__":
     app.run(debug=True)  
