b4fa5944d5fa139519158d9ab01de5f70ad4b256
basroy <60724444+basroy@users.noreply.github.com>
2020-02-10 14:57:09 -0800

    Update pop_table.py
    Populated the Grid with data from transposed DF.

--- pop_table.py
+++ pop_table.py
@@ -12,18 +12,40 @@ records = cursor.fetchall()
 df = pd.DataFrame(records)
 dfshape = df.shape
 griddata = df.transpose()
-#print(df)
+print(df)
+print(griddata)
 print(dfshape[0],dfshape[1])
 conn.close()
 
 EVEN_ROW_COLOUR = '#CCE6DD'
 GRID_LINE_COLOUR = '#aaa'
 
+class dsptrx(wx.Frame):
+
+    def __init__(self,parent,id):
+        wx.Frame.__init__(self,parent,id,'Bashobi frame',pos=(25,50),size=(500,300))
+        panel = wx.Panel(self)
+        button = wx.Button(panel, label="exit", pos=(130,10),size=(20,20))
+        self.Bind(wx.EVT_BUTTON, self.closebutton,button)
+        #self.Bind(wx.EVT_CLOSE, self.closewindow)
+
+    def closebutton(self,event):
+        self.Close(True)
+
+    def closewindow(self,event):
+        self.Destroy(True)        
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = dsptrx(parent=None, id=-1)
+    frame.Show()
+    app.MainLoop()
+
+
 app = wx.App()
 frame = wx.Frame(None,title='Bashobi frame',pos=(25,50),size=(500,300))
 panel = wx.Panel(frame)
-button = wx.Button(panel, label="exit", pos=(130,10),size=(20,20)
-                   # Trying to not use Class....but dont see a way. 
+button = wx.Button(panel, label="exit", pos=(130,10),size=(20,20))
 
 menuBar = wx.MenuBar()
 fileButton = wx.Menu()
@@ -33,6 +55,13 @@ menuBar.Append(editButton, 'Edit')
 
 grid = gridlib.Grid(panel)
 grid.CreateGrid(dfshape[0],dfshape[1]) #change to make all data from datagrid fit, makew this fit any query
+
+columns = dfshape[0]
+rows = dfshape[1]
+for i in range(0,columns):
+    for j in range(0,rows):
+        grid.SetCellValue(i,j,str(df[j][i]))
+
 sizer = wx.BoxSizer(wx.VERTICAL)
 sizer.Add(grid, 1,wx.EXPAND)
 panel.SetSizer(sizer)
@@ -43,5 +72,6 @@ panel.SetSizer(sizer)
 griddata.aggregate
 frame.Show()
 app.MainLoop()
-print(len(griddata))
-print(len(griddata.columns))
+#print(len(griddata))
+#print(len(griddata.columns))
+
