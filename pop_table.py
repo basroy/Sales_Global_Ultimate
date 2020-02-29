e8f5f5d901fb97b1b10ddcffa3cf4239e2b2fecb
basroy <60724444+basroy@users.noreply.github.com>
2020-02-28 16:02:37 -0800

    Add files via upload
    Text Box to accept input. Exit Button and Exit Menu Item

--- pop_table.py
+++ pop_table.py
@@ -12,22 +12,41 @@ records = cursor.fetchall()
 df = pd.DataFrame(records)
 dfshape = df.shape
 griddata = df.transpose()
-print(df)
-print(griddata)
-print(dfshape[0],dfshape[1])
+#print(df)
+#print(griddata)
+#print(dfshape[0],dfshape[1])
 conn.close()
 
 EVEN_ROW_COLOUR = '#CCE6DD'
 GRID_LINE_COLOUR = '#aaa'
 
+class DataPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent = parent)
+
 class dsptrx(wx.Frame):
 
     def __init__(self,parent,id):
-        wx.Frame.__init__(self,parent,id,'Bashobi frame',pos=(25,50),size=(500,300))
+        wx.Frame.__init__(self,parent,id,'Bashobi frame',pos=(55,30),size=(500,300))
         panel = wx.Panel(self)
-        button = wx.Button(panel, label="exit", pos=(130,10),size=(20,20))
+        button = wx.Button(panel, label="exit", pos=(230,110),size=(50,30))
         self.Bind(wx.EVT_BUTTON, self.closebutton,button)
-        #self.Bind(wx.EVT_CLOSE, self.closewindow)
+        menuBar = wx.MenuBar()
+        filemenu = wx.Menu()
+        editmenu = wx.Menu()
+        filemenu.Append(wx.ID_FILE1, "New Window","Open new terminal")
+        # filemenu.Append(wx.NewId(), "Save","Save file")
+        #filemenu.Append(wx.ID_EXIT, "Exit","Next query run")
+        exitItem = filemenu.Append(wx.ID_EXIT, "Exit","Next query run")
+        menuBar.Append(filemenu, "File")
+        menuBar.Append(editmenu, "Edit")
+        self.SetMenuBar(menuBar)
+        self.Bind(wx.EVT_MENU, self.closebutton, exitItem)
+        grid = gridlib.Grid(panel)
+        grid.CreateGrid(dfshape[0],dfshape[1])
+        box = wx.TextEntryDialog(None, "Enter a SQL Script")
+        if box.ShowModal() == wx.ID_OK:
+            script = box.GetValue()
 
     def closebutton(self,event):
         self.Close(True)
@@ -45,13 +64,6 @@ if __name__ == '__main__':
 app = wx.App()
 frame = wx.Frame(None,title='Bashobi frame',pos=(25,50),size=(500,300))
 panel = wx.Panel(frame)
-button = wx.Button(panel, label="exit", pos=(130,10),size=(20,20))
-
-menuBar = wx.MenuBar()
-fileButton = wx.Menu()
-editButton = wx.Menu()
-menuBar.Append(fileButton, 'File')
-menuBar.Append(editButton, 'Edit')
 
 grid = gridlib.Grid(panel)
 grid.CreateGrid(dfshape[0],dfshape[1]) #change to make all data from datagrid fit, makew this fit any query
@@ -66,12 +78,7 @@ sizer = wx.BoxSizer(wx.VERTICAL)
 sizer.Add(grid, 1,wx.EXPAND)
 panel.SetSizer(sizer)
 
-#Populate the Panel from df
-#table = wx.grid.PyGridTableBase(df)
-
 griddata.aggregate
 frame.Show()
 app.MainLoop()
-#print(len(griddata))
-#print(len(griddata.columns))
 
