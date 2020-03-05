0660babc2233973f4e7246e157444c70c54a2975
basroy <60724444+basroy@users.noreply.github.com>
2020-03-04 20:07:02 -0800

    MENUBAR not working with GRID
    Can't get GRID displayed properly with MenuBar :( :(

--- pop_table.py
+++ pop_table.py
@@ -4,27 +4,6 @@ import wx
 import wx.grid as gridlib
 
 
-conn = cx_Oracle.connect('XXG2CRO/C1sc0bea7@G2CPRD')
-cursor = conn.cursor()
-cursor.execute('Select trx_id, trx_number, Source_trx_id, otm_date, creation_date, last_update_date, deal_id \
-                   , trx_usd_value  , source_trx_version_id , otm_batch_id, trx_applied_line_id \
-                   from xxotm.xxotm_phx_trx where rownum <= 10')
-records = cursor.fetchall()
-df = pd.DataFrame(records)
-dfshape = df.shape
-griddata = df.transpose()
-#print(df)
-#print(griddata)
-#print(dfshape[0],dfshape[1])
-conn.close()
-
-EVEN_ROW_COLOUR = '#CCE6DD'
-GRID_LINE_COLOUR = '#aaa'
-
-class DataPanel(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent = parent)
-
 class dsptrx(wx.Frame):
 
     def __init__(self,parent,id):
@@ -35,16 +14,48 @@ class dsptrx(wx.Frame):
         box = wx.TextEntryDialog(None, "Enter a SQL Script")
         if box.ShowModal() == wx.ID_OK:
             script = box.GetValue()
+
+        buttonEXIT = wx.Button(panel, label="exit", pos=(230,110),size=(50,30))
+        self.Bind(wx.EVT_BUTTON, self.closebutton,buttonEXIT)
+
+        menuBar = wx.MenuBar()
+        filemenu = wx.Menu()
+        editmenu = wx.Menu()
+        filemenu.Append(wx.ID_FILE1, "New Window","Open new terminal")
+        exitItem = filemenu.Append(wx.ID_EXIT, "Exit","Next query run")
+        menuBar.Append(filemenu, "File")
+        menuBar.Append(editmenu, "Edit")
+        
+        sizerMENU =  wx.BoxSizer(wx.HORIZONTAL)
+        sizerMENU.Add(menuBar, 0, wx.CENTER|wx.ALL, 5)
+ 
+        #self.SetMenuBar(menuBar)
+       # self.Bind(wx.EVT_MENU, self.closebutton, exitItem)
+        
         df = self.trxquery()
-        print(df)
+        #print(df)
         dfshape = df.shape
         griddata = df.transpose()
         grid = gridlib.Grid(panel)
         grid.CreateGrid(dfshape[0],dfshape[1])
+        EVEN_ROW_COLOUR = '#CCE6DD'
+        GRID_LINE_COLOUR = '#aaa'
+        grid.SetColLabelValue(0, "TRX_ID")
+        grid.SetColLabelValue(1, "TRX_NUMBER")
+        grid.SetColLabelValue(2, "SOURCE_TRX_ID")
+        grid.SetColLabelValue(3, "OTM_DATE")
+        grid.SetColLabelValue(4, "CREATION_DATE")
+        grid.SetColLabelValue(5, "LAST_UPDATE_DATE")
+        grid.SetColLabelValue(6, "DEAL_ID")
+        grid.SetColLabelValue(7, "TRX_USD_VALUE")
+        grid.SetColLabelValue(8, "SOURCE_TRX_VERSION_ID")
+        grid.SetColLabelValue(9, "OTM_BATCH_ID")
+        grid.SetColLabelValue(10, "TRX_APPLIED_LINE_ID")
 
-        buttonqry  = wx.Button(panel, label="Query Results")
+        buttonqry  = wx.Button(panel, label="Query Results export")
         sizerqry =  wx.BoxSizer(wx.HORIZONTAL)
         sizerqry.Add(buttonqry, 0, wx.CENTER|wx.ALL, 5)
+        sizerqry.Add(buttonEXIT, 0, wx.CENTER|wx.ALL, 5)
         sizerqry.AddSpacer(10)
         
         sizerdata = wx.BoxSizer(wx.VERTICAL)
@@ -73,6 +84,7 @@ class dsptrx(wx.Frame):
                    from xxotm.xxotm_phx_trx where rownum <= 10')
         records = cursor.fetchall()
         df = pd.DataFrame(records)
+        conn.close()
         return df
        
 
@@ -81,26 +93,3 @@ if __name__ == '__main__':
     frame = dsptrx(parent=None, id=-1)
     frame.Show()
     app.MainLoop()
-    
-
-app = wx.App()
-frame = wx.Frame(None,title='Bashobi frame',pos=(25,50),size=(500,300))
-panel = wx.Panel(frame)
-
-grid = gridlib.Grid(panel)
-grid.CreateGrid(dfshape[0],dfshape[1]) #change to make all data from datagrid fit, makew this fit any query
-
-columns = dfshape[0]
-rows = dfshape[1]
-for i in range(0,columns):
-    for j in range(0,rows):
-        grid.SetCellValue(i,j,str(df[j][i]))
-
-sizer = wx.BoxSizer(wx.VERTICAL)
-sizer.Add(grid, 1,wx.EXPAND)
-panel.SetSizer(sizer)
-
-griddata.aggregate
-frame.Show()
-app.MainLoop()
-
